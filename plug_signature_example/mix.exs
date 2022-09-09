0a9c234eb642c9381ffe32bbff95c61dd1cd8573
Bram Verburg <bram.verburg@voltone.net>
2022-09-09 11:26:43 +0200

    Update plug_signature_example versions

--- plug_signature_example/mix.exs
+++ plug_signature_example/mix.exs
@@ -4,8 +4,8 @@ defmodule PlugSignatureExample.MixProject do
   def project do
     [
       app: :plug_signature_example,
-      version: "0.1.0",
-      elixir: "~> 1.8",
+      version: "0.2.0",
+      elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
